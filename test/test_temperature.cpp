39d1b38417ca165fec9072462d59cf2647447dce
Thomas Eastman <tjeastman@gmail.com>
2021-02-07 13:09:21 -0800

    update TemperatureSensorReading tests to call source-specific methods

--- test/test_temperature.cpp
+++ test/test_temperature.cpp
@@ -38,41 +38,41 @@ void test_temperature_sensor_reading_valid() {
 }
 
 void test_temperature_sensor_reading_temperature() {
-  TEST_ASSERT_EQUAL_FLOAT(
-      1600.0, TemperatureSensorReading(0x64000000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      1000.0, TemperatureSensorReading(0x3e800000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      100.75, TemperatureSensorReading(0x064c0000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      25.0, TemperatureSensorReading(0x01900000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      0.0, TemperatureSensorReading(0x00000000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      -0.25, TemperatureSensorReading(0xfffc0000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      -1.0, TemperatureSensorReading(0xfff00000).temperatures().first);
-  TEST_ASSERT_EQUAL_FLOAT(
-      -250.0, TemperatureSensorReading(0xf0600000).temperatures().first);
+  TEST_ASSERT_EQUAL_FLOAT(1600.0,
+                          TemperatureSensorReading(0x64000000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(1000.0,
+                          TemperatureSensorReading(0x3e800000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(100.75,
+                          TemperatureSensorReading(0x064c0000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(25.0,
+                          TemperatureSensorReading(0x01900000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(0.0,
+                          TemperatureSensorReading(0x00000000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(-0.25,
+                          TemperatureSensorReading(0xfffc0000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(-1.0,
+                          TemperatureSensorReading(0xfff00000).temperature());
+  TEST_ASSERT_EQUAL_FLOAT(-250.0,
+                          TemperatureSensorReading(0xf0600000).temperature());
 }
 
-void test_temperature_sensor_reading_internal_temperature() {
+void test_temperature_sensor_reading_temperature_internal() {
   TEST_ASSERT_EQUAL_FLOAT(
-      127.0, TemperatureSensorReading(0x00007f00).temperatures().second);
+      127.0, TemperatureSensorReading(0x00007f00).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      100.5625, TemperatureSensorReading(0x00006490).temperatures().second);
+      100.5625, TemperatureSensorReading(0x00006490).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      25.0, TemperatureSensorReading(0x00001900).temperatures().second);
+      25.0, TemperatureSensorReading(0x00001900).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      0.0, TemperatureSensorReading(0x00000000).temperatures().second);
+      0.0, TemperatureSensorReading(0x00000000).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      -0.0625, TemperatureSensorReading(0x0000fff0).temperatures().second);
+      -0.0625, TemperatureSensorReading(0x0000fff0).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      -1.0, TemperatureSensorReading(0x0000ff00).temperatures().second);
+      -1.0, TemperatureSensorReading(0x0000ff00).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      -20.0, TemperatureSensorReading(0x0000ec00).temperatures().second);
+      -20.0, TemperatureSensorReading(0x0000ec00).temperature_internal());
   TEST_ASSERT_EQUAL_FLOAT(
-      -55.0, TemperatureSensorReading(0x0000c900).temperatures().second);
+      -55.0, TemperatureSensorReading(0x0000c900).temperature_internal());
 }
 
 void setup() {
@@ -80,7 +80,7 @@ void setup() {
   RUN_TEST(test_temperature_sensor_reading_status);
   RUN_TEST(test_temperature_sensor_reading_valid);
   RUN_TEST(test_temperature_sensor_reading_temperature);
-  RUN_TEST(test_temperature_sensor_reading_internal_temperature);
+  RUN_TEST(test_temperature_sensor_reading_temperature_internal);
   UNITY_END();
 }
 
